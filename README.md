3e2a92041cf772414d1333638a43333becf8b045
spidermana <690015896@qq.com>
2020-11-30 10:05:16 +0800

    final_data

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Nested Squarified Treemap
-I realize the squarified treemap of both nested and no-nested form  based on [Mark Bruls paper](https://www.win.tue.nl/~vanwijk/stm.pdf) 
+I realize the squarified treemap of both nested and non-nested form  based on [Mark Bruls paper](https://www.win.tue.nl/~vanwijk/stm.pdf) 
 
 You can find this from the dictionary named  `treemap_algorithm` .
 
-Besides, I accomplish few interactions of treemap based on `d3` to layout China Provinces. Try it in the dictionary named  `treemap_interaction` .
+Besides, I accomplish few interactions of treemap based on `d3` to layout Chinese Provinces. Try it in the dictionary named  `treemap_interaction` .
--- treemap_interaction/show_treemap_and_info.js
+++ treemap_interaction/show_treemap_and_info.js
@@ -9,36 +9,32 @@ var svg = d3.select("#vis_treemap")
             .append("g")
             .attr("transform",
                   "translate(" + margin.left + "," + margin.top + ")");
-          
-// read json data
+
+                  
 d3.json("https://raw.githubusercontent.com/spidermana/squarified-treemap-nested-and-no-nested-/main/treemap_interaction/files/China_data.json", function(data) {
   console.log(data)
-// Give the data to this cluster layout:
-var root = d3.hierarchy(data).sum(function(d){ return d.value}) // Here the size of each leave is given in the 'value' field in input data
+
+var root = d3.hierarchy(data).sum(function(d){ return d.value})
 console.log(root)
-// Then d3.treemap computes the position of each element of the hierarchy
+
+
 d3.treemap()
   .size([width, height])
-  .paddingTop(35)         //由于padding的缘故，显示出来的每个框都不止那么大。
+  .paddingTop(35)        
   .paddingRight(20)
-  .paddingInner(3)      // Padding between each rectangle
+  .paddingInner(3)      
   //.paddingOuter(6)
   //.padding(20)
   (root)
 
-// prepare a color scale
-/*
-var color = d3.scaleOrdinal() //字符串和值的映射
-  .domain(["boss1", "boss2", "boss3"])
-  .range([ "#402D54", "#D18975", "#8FD175"])
-*/
+
 var color = d3.scaleOrdinal(d3.schemeCategory20)
 // And a opacity scale
 var opacity = d3.scaleLinear()
   .domain([10, 90])   //value
   .range([.5,1])  //0.5~1的透明度
 
-// use this information to add rectangles:
+
 svg
 .selectAll("rect")
 .data(root.leaves())
@@ -53,7 +49,7 @@ svg
 .style("opacity", function(d){ return opacity(d.data.value)})
 .attr("text",function(d){ return d.data.name})
 
-// and to add the text labels
+
 svg
 .selectAll("text")
 .data(root.leaves())
@@ -62,14 +58,13 @@ svg
 .text(function(d){ return d.data.name})
 .attr("font-size", "20px")
 .attr("fill", "white")
-//.attr("x", function(d){ return d.x0+5})    // +10 to adjust position (more right)
-//.attr("y", function(d){ return d.y0+20})    // +20 to adjust position (lower)
+//.attr("x", function(d){ return d.x0+5})    
+//.attr("y", function(d){ return d.y0+20})   
 .attr("text-anchor","left")
 .attr("transform", function(d){ if(d.x1 - d.x0 < d.y1-d.y0) return "translate("+(d.x0+8)+","+(d.y0+20)+")"+"rotate(90)"; else return  "translate("+(d.x0+8)+","+(d.y0+20)+")"+"rotate(0)";})
 .style("visibility", function(d){ if((d.x1 - d.x0)/14 < d.data.name.length && ((d.y1 - d.y0)/14 < d.data.name.length)) return "hidden"; else return "visible";})
 
 
-// Add title for the 3 groups
 svg
   .selectAll("titles")
   .data(root.descendants().filter(function(d){return d.depth==1}))
